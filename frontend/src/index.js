6afaf0daad914b1959d69f257d2d22397c28d2e2
mmbarker <michellemariebarker@gmail.com>
2022-11-06 23:58:29 +0800

    6-11-22

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
